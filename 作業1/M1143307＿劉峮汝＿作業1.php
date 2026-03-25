49ca40365200035dc3baf078099a38cf889fc38a
CYUNRU <liuru0603@gmail.com>
2026-03-26 00:19:18 +0800

    Add files via upload

---
+++ 作業1/M1143307＿劉峮汝＿作業1.php
@@ -0,0 +1,70 @@
+<html>
+<head>
+<h1><font color="LIGHTSLATEGRAY">2026 核心覺醒：皮拉提斯夏令營</font></h1>
+<img src="https://www.gymco.com.tw//uploads//2023-02/14/_1676341901_32sqyr.jpg"alt="Pilates" width="580" ></img><img src="https://blog.worldgymtaiwan.com/hubfs/%E6%96%87%E7%AB%A0%E5%B0%88%E7%94%A8%E5%9C%96%E7%89%87/%E5%99%A8%E6%A2%B0%E7%9A%AE%E6%8B%89%E6%8F%90%E6%96%AF%E6%98%AF%E4%BB%80%E9%BA%BC-1.jpg"></img></a>
+</head>
+<body>
+<h3>這個夏天，給自己一個重塑體態的機會！不論你是久坐族還是運動愛好者，透過專業皮拉提斯，我們將帶領你找回身體的平衡與核心力量。</h3>
+
+<ul>課程亮點：</br>
+<li>專業指導： 從基礎呼吸到進階核心控制。<br/>
+<li>體態雕塑： 改善圓肩、駝背，練出優雅線條。<br/>
+<li>身心放鬆： 結合伸展與靜心，釋放日常生活壓力。<br/>
+</ul>
+
+<h3>夏令營每日行程表</h3>
+<table border="1" width="80%">
+    <tr bgcolor="#E6E6FA"> <th>時間</th>
+        <th>第一天：核心基礎</th>
+        <th>第二天：體態雕塑</th>
+        <th>第三天：身心修復</th>
+    </tr>
+    <tr>
+        <td>09:00 - 10:30</td>
+        <td>呼吸法練習</td>
+        <td>背部線條強化</td>
+        <td>全身深層伸展</td>
+    </tr>
+    <tr>
+        <td>10:30 - 12:00</td>
+        <td>基礎核心啟動</td>
+        <td>下肢平衡訓練</td>
+        <td>冥想與放鬆</td>
+    </tr>
+</table>
+<h3>學員行前準備</h3>
+<ul>
+    <li>個人瑜珈墊（教室亦有提供）</li>
+    <li>透氣運動服飾</li>
+    <li>水壺與毛巾</li>
+    <li>一顆放鬆的心</li>
+</ul>
+<h2>活動報名表單</h2>
+
+ 姓名：<input type="text" name="userName" placeholder="請輸入全名" ><br/>
+
+ 信箱：<input type="email" name="userEmail"><br/>
+
+ 性別：
+    <input type="radio" name="gender" value="m">男
+    <input type="radio" name="gender" value="f" checked>女<br/>
+
+想參加的班別：
+    <select name="camp_class">
+    <option value="basic">基礎核心班</option>
+    <option value="advanced">進階體態班</option>
+    <option value="weekend">週末放鬆營</option> </select>
+    <br/>
+
+過去運動經驗：
+    <input type="checkbox" name="exp" value="yoga">瑜珈
+    <input type="checkbox" name="exp" value="gym">健身
+    <input type="checkbox" name="exp" value="none">無經驗<br/><br/>
+
+身體狀況說明：<br/>
+    <textarea name="health_note" rows="4" cols="50">是否有受傷紀錄或特殊需求？</textarea><br/><br/>
+
+    <input type="submit" value="立即報名">
+    <input type="reset" value="重新填寫">
+</body>
+</html>
