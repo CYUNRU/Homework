aa33a31414df25a000973f9bc5c62f26b6cd48ce
CYUNRU <liuru0603@gmail.com>
2026-04-17 00:30:58 +0800

    Add files via upload

---
+++ 作業3/admin.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+
+if(isset($_SESSION['login'])){
+   if($_SESSION['login']=='admin'){
+   echo "<h1>Welcome! Login Success</h1></br>";
+   echo "<a href='logout.php'>Logout</a>";
+   }else{
+   echo "<h1>非法進入你會看不到東西! 2秒後回到登入頁面</h1>";
+   header("Refresh:3;url=login.php");
+   }
+}else{
+    echo "<h1>非法進入你會看不到東西!2秒後回到登入頁面</h1>";
+    header("Refresh:3;url=login.php");
+}
+?>
